8d9239ede94346917d78112d28680b5df117c1a7
CEE <archmagece@gmail.com>
2020-11-22 16:03:39 +0900

    test model insert

--- src/main/kotlin/models.kt
+++ src/main/kotlin/models.kt
@@ -1,26 +1,24 @@
 import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.jodatime.datetime
-import org.jetbrains.exposed.sql.money.currency
-import org.joda.time.DateTime
 
 
 object Scatter : LongIdTable(name = "t_scatter") {
-    val roomId: Column<Long> = long("room_id")
-    val userId: Column<Long> = long("user_id")
-    val token: Column<String> = varchar("token", 3).uniqueIndex("idx_ppurigi_token_uniq")
+    val roomId = long("room_id")
+    val userId = long("user_id")
+    val token = varchar("token", 3).uniqueIndex("idx_ppurigi_token_uniq")
 
     val createdAt = datetime("created_at")
 
     //    val amount = currency("currency")
-    val totalAmountOfMoney = integer("amount_of_money")
-    val totalNumberOfPeople = integer("number_of_people")
+    val totalAmountOfMoney = long("amount_of_money")
+    val totalNumberOfPeople = long("number_of_people")
 }
 
 object Treasure : LongIdTable(name = "t_treasure") {
     val scatter = long("scatter").references(Scatter.id, onDelete = ReferenceOption.CASCADE)
 
-    val amount = integer("amount")
+    val amount = long("amount")
 }
 
 object Gather : LongIdTable(name = "t_gather") {
---
+++ src/test/kotlin/test-repository.kt
@@ -0,0 +1,64 @@
+import com.typesafe.config.ConfigFactory
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.transactions.transactionManager
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import java.util.*
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class TestCRUD {
+    val db = initDB()
+
+    @BeforeTest
+    fun before(){
+        val dbType = ConfigFactory.load().getString("db_type")
+        val config = ConfigFactory.load().getConfig(dbType)
+        val properties = Properties()
+        config.entrySet().forEach { e -> properties.setProperty(e.key, config.getString(e.key)) }
+        val hikariConfig = HikariConfig(properties)
+        val ds = HikariDataSource(hikariConfig)
+        Database.connect(ds).apply {
+            useNestedTransactions = true
+        }
+        transaction {
+            SchemaUtils.create (Scatter, Treasure, Gather)
+        }
+    }
+
+    @Test
+    fun `test ppurigi model insert`() {
+        transaction {
+            val pRoomId = 1L
+            val pUserId = 1L
+            val pToken = "AAA"
+            val pTotalAmountOfMoney = 10000L
+            val pTotalNumberOfPeople = 3L
+            val scatterId1 = Scatter.insertAndGetId {
+                it[roomId] = pRoomId
+                it[userId] = pUserId
+                it[token] = pToken
+                it[createdAt] = DateTime.now(DateTimeZone.UTC)
+                it[totalAmountOfMoney] = pTotalAmountOfMoney
+                it[totalNumberOfPeople] = pTotalNumberOfPeople
+            }
+            println(pTotalAmountOfMoney / pTotalNumberOfPeople)
+            val amountForTreasure = pTotalAmountOfMoney / pTotalNumberOfPeople
+            val treasureId1 = Treasure.insert {
+                it[scatter] = scatterId1.value
+                it[amount] = amountForTreasure
+            } get Treasure.id
+            Gather.insert {
+                it[scatter] = scatterId1.value
+                it[treasure] = treasureId1.value
+                it[roomId] = pRoomId
+                it[userId] = pUserId
+                it[createdAt] = DateTime.now(DateTimeZone.UTC)
+            }
+        }
+    }
+
+}
